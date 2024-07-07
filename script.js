f0916c2e4c2ce362c5b4504f5bfea0d716d01410
nadi726 <16650084+nadi726@users.noreply.github.com>
2024-07-07 16:40:56 +0300

    Add buttons for setting and resetting grid size
    Required a refactoring of the script, resulting
    in much cleaner code. Main part of the project is
    at this point complete - everything else is extra.

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-const GRID_SIZE = 16;
+const INITIAL_GRID_SIZE = 16;
 const grid = document.querySelector("#grid");
+let size_btn = document.querySelector("#set-size-btn");
+let reset_btn = document.querySelector("#reset-grid-btn");
+let grid_size = INITIAL_GRID_SIZE;
 
-for (let col = 0; col < GRID_SIZE; col++) {
-    let rowDiv = document.createElement("div");
-    rowDiv.className = "row";
-    for (let row = 0; row < GRID_SIZE; row++) {
-        rowDiv.appendChild(createCell());
+function createGrid(size) {
+    clearGrid()
+
+    for (let col = 0; col < size; col++) {
+        let rowDiv = document.createElement("div");
+        rowDiv.className = "row";
+        for (let row = 0; row < size; row++) {
+            rowDiv.appendChild(createCell());
+        }
+        grid.appendChild(rowDiv);
     }
-    grid.appendChild(rowDiv);
+
+    grid_size_display = document.querySelector("#grid-size-display");
+    grid_size_display.textContent = `${size}x${size}`;
+}
+
+function clearGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+function resetGrid() {
+    createGrid(grid_size);
+}
+
+function setGridSize() {
+    let newSize = prompt("Enter new size: 1-100: ")
+
+    newSize = parseInt(newSize);
+    if (Number.isNaN(newSize) || newSize > 100 || newSize < 1){
+        alert("Invalid size.")
+        return;
+    }
+    
+    grid_size = newSize;
+    createGrid(grid_size);
 }
 
 function createCell() {
@@ -21,3 +54,7 @@ function createCell() {
 function getCellColor() {
     return "black";
 }
+
+createGrid(grid_size);
+size_btn.addEventListener("click", setGridSize)
+reset_btn.addEventListener("click", resetGrid)
